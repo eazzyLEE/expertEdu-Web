a5b3e4e1422d63950a9a3d4b5e31bde752db2141
Israel Ohue <ohueisrael@yahoo.com>
2017-12-06 19:47:09 +0000

    Debugged file(mailer) error

--- app/mailers/contact_mailers.rb
+++ app/mailers/contact_mailers.rb
@@ -6,7 +6,7 @@ class Contact_mailer < Action Mailer::Base
     @email = email
     @body = body
     
-    mail (from: email, subject: 'Contact Form Message')
+    mail(from: email, subject: 'Contact Form Message')
     
   end
   
